0645a8da4bc5adb20661bb31bf2e705fd2464cf5
Bryan Galindo <galindo.bryan08@gmail.com>
2025-05-15 09:55:26 -0400

    fix type error from incorrect input param

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,3 @@
-from appwrite.client import Client
 from appwrite.exception import AppwriteException
 
 
@@ -11,6 +10,7 @@ def main(context):
         context.log(f"Received blob_text: {blob_text}")
         tx_hash = "0xef2e65640216c75332aac88cfd8beb1892b9150e3adbcf24c2ff2166c2d04dcb"
         result = {
+            "status_code": 200,
             "submitted_value": blob_text,
             "tx_hash": tx_hash
         }
@@ -19,7 +19,16 @@ def main(context):
 
     except AppwriteException as err:
         context.error("Appwrite error: " + repr(err))
-        return context.res.json({"error": "Appwrite-related issue occurred."}, status_code=500)
+        return context.res.json(
+            {"error": "Appwrite-related issue occurred.",
+             'status_code': 500
+             }
+        )
     except Exception as e:
         context.error("Unhandled error: " + str(e))
-        return context.res.json({"error": str(e)}, status_code=500)
+        return context.res.json(
+            {
+                "error": str(e),
+                'status_code': 500
+            }
+        )
